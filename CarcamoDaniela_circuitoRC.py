de9993ec7666c0e243f070c7babb114cf3828474
DanielaPosada <d.carcamo10@uniandes.edu.co>
2018-07-18 12:53:20 -0500

    RuntimeWarning2

--- CarcamoDaniela_circuitoRC.py
+++ CarcamoDaniela_circuitoRC.py
@@ -78,7 +78,12 @@ for i in range(itera):
 plt.figure()
 plt.scatter(R1,C1)
 #plt.scatter(archivo[:,0],archivo[:,1])
+plt.figure()
+plt.scatter(R1,-np.log(L1))
+plt.figure()
+plt.scatter(C1,-np.log(L1))
 plt.show()
+
 #plt.savefig("CargaRC.pdf")
 
 
