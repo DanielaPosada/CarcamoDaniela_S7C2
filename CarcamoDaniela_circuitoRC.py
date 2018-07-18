13739b4a38c84bdedf1b78b27a4e5f4a7dcfbd33
DanielaPosada <d.carcamo10@uniandes.edu.co>
2018-07-18 11:54:23 -0500

    Inicio

--- CarcamoDaniela_circuitoRC.py
+++ CarcamoDaniela_circuitoRC.py
@@ -10,15 +10,41 @@ RCal=np.ones(N[0])
 qmax=v0*C
 chi=0
 nuevo=0
-def Chi(calculado,original,chi):
+
+def Chi(calculado,original,chin):
 	for i in range(N[0]):
-		nuevo=(calculado[i]-original[i,1])**2
-		chi=nuevo+chi
-	return chi
-L=0	
+		nuevo=(calculado[i]-original[i])**2
+		chin=nuevo+chin
+	return chin
+
 def El(chi):
-	np.exp(-0.5*chi)
+	L=np.exp(-0.5*chi)
 	return L
+
+def nuevoModelo(tiempo,R,C):
+	y=qmax*(1-np.exp(-tiempo/(R*C)))
+	return y
+
+
+itera=1000
+R1=np.ones(itera)
+C1=np.ones(itera)
+L1=np.ones(itera)
+j=0
+while(j<itera):
+	R1[j]=random.random(0,20)
+	C1[j]=random.random(0,20)
+	j+=1
+j=0
+
+yinicial=np.ones(N[0])
+for i in range(N[0]):
+	yinicial[i]=nuevoModelo(archivo[i,0],R1[0],C1[0])
+
+chi1=Chi(yinicial,archivo[:,1],chi)
+L1[0]=El(chi1)
+
+
 
 
 
